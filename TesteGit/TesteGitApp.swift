dd3535fcabbde51afec63cf75355b23479a1ea62
Andreza Paiva <andrezasopaiva@gmail.com>
2022-10-11 15:39:25 -0300

    Initial Commit

---
+++ TesteGit/TesteGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteGitApp.swift
+//  TesteGit
+//
+//  Created by Andreza Paiva on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TesteGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
